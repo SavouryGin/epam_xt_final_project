3b8a0a63b30d5b94550d1ce0baa4047ccc763213
Dmitriy Surovyagin <savourygin@gmail.com>
2020-10-13 16:51:47 +0400

    Add LogIn/LogOut system

---
+++ Structural_Proof_System/PL.Web/Scripts/AppLogIn.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(onReady);
+
+function onReady() {
+    $('#loginButton').click(logIn);
+    $('#registrationButton').click(register);
+    $('#logoutButton').click(logOut);
+
+    $(document).keydown(function (event) {
+        if (event.keyCode === 13) {
+            $('#loginButton').click();
+        }
+    });
+}
+
+function register() {
+    $.post("/Pages/processSingIn.cshtml",
+        {
+            Login: $('#login').val(),
+            Password: $.md5($('#password').val())
+        }, function (data) {
+            $('#result').html(data);
+        });
+}
+
+function logIn() {
+    $.post("/Pages/processLogIn.cshtml",
+        {
+            Login: $('#login').val(),
+            Password: $.md5($('#password').val())
+        }, function (data) {
+            if (data == "")
+                window.location.href = "/";
+            else
+                $('#result').html(data);
+        });
+}
+
+function logOut() {
+    window.location.href = "logout.cshtml";
+}
